9604187f7260ac849f28d3669e426e027efa4d3d
EMCO08 <emirhanalizaferr@hotmail.com>
2025-04-05 03:59:42 +0300

    heroku bes

--- notlar/settings.py
+++ notlar/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-os@pt_60+c9w6o52_zp6c_7vim8xti1u6&km^vd2$sb!sts9*^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = false
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
